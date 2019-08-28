811e958a976f154d48e04a9b1b820b1608abdcdb
Benjamin David <ben@dukt.net>
2019-08-28 23:34:12 +0200

    Removed developer support API endpoint mocking

--- web/craftnetresources/id/src/js/api/developer-support.js
+++ web/craftnetresources/id/src/js/api/developer-support.js
@@ -4,35 +4,7 @@ import axios from 'axios'
 import qs from 'qs'
 
 export default {
-    mockedSubscriptionInfo(currentPlan) {
-        return {
-            currentPlan,
-            pro: {
-                uid: 1234,
-                canceled: false,
-                cycleEnd: '2019-09-19',
-            },
-            premium: {
-                uid: 4567,
-                canceled: false,
-                cycleEnd: '2019-09-19',
-                upgradeCost: 412.5,
-            },
-        }
-    },
-
     getSubscriptionInfo() {
-        // return new Promise((resolve, reject) => {
-        //     setTimeout(function() {
-        //         // reject('Some error')
-        //         // return null
-        //
-        //         const response = this.mockedSubscriptionInfo('pro')
-        //
-        //         resolve(response)
-        //     }.bind(this), 2000)
-        // })
-
         return axios.get(Craft.actionUrl + '/craftnet/id/developer-support/get-subscription-info', {}, {
             headers: {
                 'X-CSRF-Token': Craft.csrfTokenValue,
@@ -41,17 +13,6 @@ export default {
     },
 
     switchPlan(newPlan) {
-        // return new Promise((resolve, reject) => {
-        //     setTimeout(function() {
-        //         // reject('Some error')
-        //         // return null
-        //
-        //         const response = this.mockedSubscriptionInfo(newPlan)
-        //
-        //         resolve(response)
-        //     }.bind(this), 2000)
-        // })
-
         const data = {
             newPlan
         }
