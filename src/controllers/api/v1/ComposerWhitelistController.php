72162b92d11250f2b24964576fcfbf1433a8d556
Brandon Kelly <brandon@pixelandtonic.com>
2019-09-08 10:44:36 -0700

    Add danielstjules/stringy to the whitelit

--- src/controllers/api/v1/ComposerWhitelistController.php
+++ src/controllers/api/v1/ComposerWhitelistController.php
@@ -31,6 +31,7 @@ class ComposerWhitelistController extends BaseApiController
 
         $whitelist = [
             'craftcms/vue-asset' => true,
+            'danielstjules/stringy' => true,
         ];
 
         $this->_ignoreDeps = [
