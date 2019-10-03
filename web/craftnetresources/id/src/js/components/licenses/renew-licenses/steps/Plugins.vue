214e85b1a3eb2da34fbf4ca54ca4ab8220d25835
Benjamin David <ben@dukt.net>
2019-10-03 20:28:40 +0200

    Prevent adding CMS or plugin renewals that are already present in the cart

--- web/craftnetresources/id/src/js/components/licenses/renew-licenses/steps/Plugins.vue
+++ web/craftnetresources/id/src/js/components/licenses/renew-licenses/steps/Plugins.vue
@@ -1,45 +1,46 @@
 <template>
     <div>
-        <spinner v-if="loading"></spinner>
+        <p>Do you want to renew plugin licenses as well?</p>
+        <table class="table mb-2">
+            <thead>
+            <tr>
+                <td><input type="checkbox" v-model="checkAllChecked" ref="checkAll" @change="checkAll"></td>
+                <th>Item</th>
+                <th>Renewal Date</th>
+                <th>New Renewal Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="(renewableLicense, key) in renewableLicenses(license, renew)" :key="key">
+                <td>
+                    <input
+                            type="checkbox"
+                            :value="1"
+                            :disabled="(key === 0 || !renewableLicense.key || alreadyInCart(renewableLicense)) ? true : false"
+                            :checked="renewableLicense.key && !alreadyInCart(renewableLicense) ? checkedLicenses[key] : false"
+                            @input="checkLicense($event, key)" />
+                </td>
+                <td :class="{'text-grey': !renewableLicense.key}">
+                    {{ renewableLicense.description }}
+                    <div v-if="alreadyInCart(renewableLicense) && !loading" class="text-grey-dark">Already in cart.</div>
+                </td>
+                <td :class="{'text-grey': !renewableLicense.key}">{{ renewableLicense.expiresOn.date|moment('YYYY-MM-DD') }}</td>
+                <td :class="{'text-grey': !renewableLicense.key}">
+                    {{ renewableLicense.expiryDate|moment('YYYY-MM-DD') }}
+                </td>
+                <td></td>
+            </tr>
+            </tbody>
+        </table>
 
-        <template v-else>
-            <p>Do you want to renew plugin licenses as well?</p>
-            <table class="table mb-2">
-                <thead>
-                <tr>
-                    <td><input type="checkbox" v-model="checkAllChecked" ref="checkAll" @change="checkAll"></td>
-                    <th>Item</th>
-                    <th>Renewal Date</th>
-                    <th>New Renewal Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="(renewableLicense, key) in renewableLicenses(license, renew)" :key="key">
-                    <td>
-                        <input
-                                type="checkbox"
-                                :value="1"
-                                :disabled="(key === 0 || !renewableLicense.key) ? true : false"
-                                :checked="renewableLicense.key ? checkedLicenses[key] : false"
-                                @input="checkLicense($event, key)" />
-                    </td>
-                    <td :class="{'text-grey': !renewableLicense.key}">{{ renewableLicense.description }}</td>
-                    <td :class="{'text-grey': !renewableLicense.key}">{{ renewableLicense.expiresOn.date|moment('YYYY-MM-DD') }}</td>
-                    <td :class="{'text-grey': !renewableLicense.key}">
-                        {{ renewableLicense.expiryDate|moment('YYYY-MM-DD') }}
-                    </td>
-                    <td></td>
-                </tr>
-                </tbody>
-            </table>
-
-            <btn @click="$emit('back')">Back</btn>
-            <btn ref="submitBtn" @click="addToCart()" kind="primary">Add to cart</btn>
-        </template>
+        <btn @click="$emit('back')">Back</btn>
+        <btn ref="submitBtn" @click="addToCart()" kind="primary">Add to cart</btn>
     </div>
 </template>
 
 <script>
+    import {mapGetters} from 'vuex'
+
     import helpers from '../../../../mixins/helpers'
 
     export default {
@@ -52,6 +53,14 @@
                 loading: false,
                 checkAllChecked: false
             }
+        },
+
+
+        computed: {
+            ...mapGetters({
+                cartItems: 'cart/cartItems',
+            }),
+
         },
 
         methods: {
@@ -75,8 +84,12 @@
 
                 if ($event.target.checked) {
                     this.renewableLicenses(this.license, this.renew).forEach(function(renewableLicense, key) {
+                        if (this.alreadyInCart(renewableLicense)) {
+                            return false
+                        }
+
                         checkedLicenses[key] = 1
-                    })
+                    }.bind(this))
                 } else {
                     checkedLicenses[0] = 1
                 }
@@ -109,16 +122,26 @@
 
                     items.push(item)
                 }.bind(this))
-                
+
+                this.loading = true
                 this.$store.dispatch('cart/addToCart', items)
                     .then(() => {
+                        this.loading = false
                         this.$router.push({path: '/cart'})
                         this.$emit('addToCart')
                     })
                     .catch((errorMessage) => {
+                        this.loading = false
                         this.$store.dispatch('app/displayError', errorMessage)
                     })
             },
+
+            alreadyInCart(renewableLicense) {
+                const licenseKey = renewableLicense.key
+                const cartItems = this.cartItems
+
+                return cartItems.find(item => item.lineItem.options.licenseKey === licenseKey)
+            }
         },
 
         mounted() {
