530dd2ed3c0dcd5e6cec7d50070a24201cc1096f
Brandon Kelly <brandon@pixelandtonic.com>
2020-06-11 16:03:37 -0700

    Only count licenses that came in the last year

--- src/console/controllers/PluginsController.php
+++ src/console/controllers/PluginsController.php
@@ -3,11 +3,10 @@
 namespace craftnet\console\controllers;
 
 use Craft;
+use craft\helpers\Db;
 use craftnet\Module;
-use craftnet\plugins\Plugin;
 use yii\console\Controller;
 use yii\console\ExitCode;
-use yii\db\Expression;
 
 /**
  * Manages plugins.
@@ -27,7 +26,10 @@ class PluginsController extends Controller
             ->createCommand('update craftnet_plugins as p set "activeInstalls" = (
     select count(*) from craftnet_cmslicense_plugins as lp
     where lp."pluginId" = p.id
-)')
+    and lp.timestamp > :date
+)', [
+                'date' => Db::prepareDateForDb(new \DateTime('1 year ago'))
+            ])
             ->execute();
 
         return ExitCode::OK;
